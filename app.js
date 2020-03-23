d5a3d595c4f4a05d2d459a493d4802e5a88c6c92
iAquino47 <ianaquino47@gmail.com>
2020-03-23 21:09:14 +0000

    basic structure done

--- app.js
+++ app.js
@@ -8,3 +8,52 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+scrores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+document.querySelector(`#current-${activePlayer}`).innerHTML = '<em>'+ dice +'</em>';
+
+let x = document.querySelector('#score-1').textContent;
+console.log(x)
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+        roundScore += dice;
+        console.log(roundScore);
+        document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+
+}) 
+
