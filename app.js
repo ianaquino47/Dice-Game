11dfd893c2a273cd37f8059309e3fe05c576f227
iAquino47 <ianaquino47@gmail.com>
2020-03-23 23:35:47 +0000

    refactored some functionality into a function

--- app.js
+++ app.js
@@ -42,17 +42,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         console.log(roundScore);
         document.getElementById(`current-${activePlayer}`).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
-        document.querySelector('.dice').style.display = 'none';
-
+        nextPlayer();
     }
 
 }) 
@@ -60,12 +50,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 document.querySelector('.btn-hold').addEventListener('click', function() {
     // Add CURRENT score to GLOBAL score
     scores[activePlayer] += roundScore
+
     // Update the UI
     document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
     
     //Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+    
 
-    //Next player
+})
+
+function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
     roundScore = 0;
 
@@ -76,5 +78,4 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('.player-1-panel').classList.toggle('active')
 
     document.querySelector('.dice').style.display = 'none';
-
-})
+}
